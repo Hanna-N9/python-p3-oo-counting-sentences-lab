a493afc8393a7011e00e96ad18a284154bf9134e
Hanna Negash <hnegash.n@gmail.com>
2023-12-12 15:08:34 -0800

    Real solution added

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
 
 class MyString:
-    def __init__(self, value=""):
-      self.value = value
+  def __init__(self, value = ""):
+    self._value = value
+  
+  @property  
+  def get_value(self):
+    return self._value
 
-    @property
-    def get_value(self):
-      return self._value
-  
-    def set_value(self, stringVal):
-      if isinstance(value, str):
-        self._value = value
-      else:
-        print("The value must be a string.")
-        
-    def is_sentence(self):
-      return self._value.endswith(".") 
-  
-    def is_question(self):
-      return self._value.endswith("?") 
-    
-    def is_exclamation(self):
-      return self._value.endswith("!") 
-    
-    def count_sentences(self):
-      value = self.value
-      for punctuation in ["!","?"]:
+  def set_value(self, stringVal):
+    if (type(stringVal) == str):
+      self._value = stringVal
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+
+  def count_sentences(self):
+    value = self.value
+    for punctuation in ["!","?"]:
         value = value.replace(punctuation, ".")
-      else:
-        return len([sentence for sentence in value.split(".") if sentence])
+        
+    return len([sentence for sentence in value.split(".") if sentence])
+    
